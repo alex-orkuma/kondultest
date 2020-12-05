d7668208d6c9397ccd79331a0758a367ecdf071d
alex-orkuma <orkumashi@gmail.com>
2020-12-05 21:13:11 +0100

    add a HelloHTTP function

--- kondultest.go
+++ kondultest.go
@@ -1,1 +1,24 @@
 package kondultest
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+)
+
+// HelloHTTP cloud function sample
+func HelloHTTP(rw http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Name string `json:"name"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(rw, "Hello World!")
+		return
+	}
+	if d.Name == "" {
+		fmt.Fprint(rw, "Hello World!")
+		return
+	}
+	fmt.Fprint(rw, "Hello", html.EscapeString(d.Name))
+}
